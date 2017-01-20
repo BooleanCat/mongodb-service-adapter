e0da9b09009d4d40fb7973c9965d2986e7600b08
BooleanCat <thgodkin@gmail.com>
2017-01-20 13:29:43 +0000

    test for exit code on generate-manifest

--- tests/test_return_codes.rs
+++ tests/test_return_codes.rs
@@ -1,7 +1,33 @@
+extern crate tempdir;
+
 #[cfg(test)]
 mod tests {
+    use tempdir::TempDir;
+    use std::process::Command;
+
     #[test]
     fn it_works() {
-        
+        let temp_dir = TempDir::new("")
+            .expect("failed to create tempdir");
+
+        let bin_path = temp_dir.path().join("mongodb-service-adapter");
+        let command = &mut Command::new("rustc");
+        let command = command
+            .arg("-o")
+            .arg(bin_path)
+            .arg("src/main.rs");
+
+        let _ = command.status()
+            .expect("failed to build");
+
+        let adapter_command = &mut Command::new(temp_dir.path().join("mongodb-service-adapter"));
+        let adapter_command = adapter_command.arg("generate-manifest");
+
+        let status = adapter_command.status()
+            .expect("failed to build");
+
+        let exit_code = status.code().unwrap();
+
+        assert_eq!(exit_code, 10);
     }
 }
